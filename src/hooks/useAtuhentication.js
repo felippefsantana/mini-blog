f68d5a24894911a971c8d35eb5122ed21531b2bd
Felippe Santana <felippefsantana02@gmail.com>
2023-03-27 16:51:36 -0300

    registering user

--- src/hooks/useAtuhentication.js
+++ src/hooks/useAtuhentication.js
@@ -1,10 +1,12 @@
+import { db } from '../firebase/config';
+
 import {
   getAuth,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   updateProfile,
   signOut
-} from 'firebase/atuh';
+} from 'firebase/auth';
 
 import { useState, useEffect } from 'react';
 
@@ -22,5 +24,52 @@ export const useAuthentication = () => {
     if (cancelled) {
       return;
     }
+  }
+
+  const createUser = async (data) => {
+    checkIfIsCancelled();
+    setLoading(true);
+    setError(null);
+
+    try {
+      const { user } = await createUserWithEmailAndPassword(
+        auth,
+        data.email,
+        data.password
+      );
+
+      await updateProfile(user, {
+        displayName: data.displayName
+      });
+
+      setLoading(false);
+
+      return user;
+
+    } catch (error) {
+      let systemErrorMessage;
+
+      if (error.message.includes('Password')) {
+        systemErrorMessage = 'A senhas precisa conter pelo menos 6 caracteres.';
+      } else if (error.message.includes('email-already')) {
+        systemErrorMessage = 'E-mail já cadastrado.';
+      } else {
+        systemErrorMessage = 'Ocorreu um erro! Por favor tente mais tarde.';
+      }
+
+      setLoading(false);
+      setError(systemErrorMessage);
+    }
+  }
+
+  useEffect(() => {
+    return () => setCancelled(true);
+  }, []);
+
+  return {
+    auth,
+    createUser,
+    error,
+    loading
   }
 }

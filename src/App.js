6471f8bb26f44a14a42943b73fd3e48e956f7284
Felippe Santana <felippefsantana02@gmail.com>
2023-03-27 14:46:20 -0300

    config react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import './App.css';
+
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+// Pages
+import Home from './pages/Home';
+import About from './pages/About';
+
 function App() {
   return (
     <div className="App">
-      <h1>MiniBlog</h1>
+      <BrowserRouter>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/about" element={<About />}></Route>
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }

0cf83c8102a7c1c6e2fac5ba44d855db497472fb
Felippe Santana <felippefsantana02@gmail.com>
2023-03-27 14:58:10 -0300

    pages and components

--- src/App.js
+++ src/App.js
@@ -3,19 +3,25 @@ import './App.css';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 // Pages
-import Home from './pages/Home';
-import About from './pages/About';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+
+// Components
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <div className="container">
           <Routes>
             <Route path="/" element={<Home />}></Route>
             <Route path="/about" element={<About />}></Route>
           </Routes>
         </div>
+        <Footer />
       </BrowserRouter>
     </div>
   );
